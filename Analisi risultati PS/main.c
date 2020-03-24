8aac5db189e6d30d8fb6cb8cf55d9d05f0fc9228
Lorenzo125 <55649413+Lorenzo125@users.noreply.github.com>
2020-03-24 11:59:55 +0100

    Add files via upload

--- Analisi risultati PS/main.c
+++ Analisi risultati PS/main.c
@@ -1,7 +1,7 @@
 int main (){
   TCanvas *c1 = new TCanvas ("c1","",1500,500);
-  c1->Divide(2,1);
-  TFile *f = TFile::Open("PS_20_5000_2_2_0.05.root", "READ");
+  c1->Divide(3,1);
+  TFile *f = TFile::Open("PS_20_1000_4_4_0.1.root", "READ");
   //TFile *f = TFile::Open("test_J_64_02.root", "READ");
   TGraphAsymmErrors *g = new TGraphAsymmErrors();
   f->GetObject("chi_trend", g);
@@ -10,17 +10,24 @@ int main (){
   g->SetFillColor(4);
   g->SetFillStyle(3005);
   g->SetTitle("#chi^{2}/NDOF trend versus n. iterations");
-  c1->cd(2);
+  c1->cd(3);
   g->Draw("a3");
   //g->Draw("same");	
-  c1->cd(1);
-  TH1F * h1 = (TH1F *) f->Get("best_chi");
+  c1->cd(2);
+  TH1F * h1 = (TH1F *) f->Get("best_R2");
   //h1->SetTitle("#chi^{2} - best #chi^{2} frequency on 50 runs");
-  h1->SetTitle("#chi^{2}/NDOF frequency on 50 runs");
+  h1->SetTitle("R^{2} frequency on 50 runs");
   h1->GetYaxis()->SetTitle("frequency");
   //h1->GetXaxis()->SetTitle("#chi^{2} - best #chi^{2}");
-  h1->GetXaxis()->SetTitle("#chi^{2}/NDOF");
+  h1->GetXaxis()->SetTitle("#R^{2}");
   h1 -> Draw("HIST");
+  c1->cd(1);
+  TH1F * h2 = (TH1F *) f->Get("best_chi");
+  h2->SetTitle("#chi^{2}/Ndof frequency on 50 runs");
+  h2->GetYaxis()->SetTitle("frequency");
+  h2->GetXaxis()->SetTitle("#chi^{2}/Ndof");
+  h2 -> Draw("HIST");
+
   /*c1->cd(2);
   TH1F * time = (TH1F *) f->Get("time");
   time->SetTitle("time required frequency on 50 runs");
